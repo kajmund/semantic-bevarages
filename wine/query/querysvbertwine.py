e996d1153a1da6416f586d16118d84a5ab09e877
Erik Fremred <erik@fremred.se>
2023-04-24 06:37:51 +0200

    mer minneseffektiv

--- wine/query/querysvbertwine.py
+++ wine/query/querysvbertwine.py
@@ -3,10 +3,10 @@ import faiss
 import pandas as pd
 from sentence_transformers import SentenceTransformer
 
-
-## This function converts the L2_score into inner product def
-def calculate_inner_product(L2_score):
-    return (2 - math.pow(L2_score, 2)) / 2
+# Ladda modell och index vid start
+model_name = 'KBLab/sentence-bert-swedish-cased'
+model = SentenceTransformer(model_name, device='cpu')
+index = faiss.read_index("./wine/wine_data_collect/movies_desc.index")
 
 
 def search_FAISSIndex(data=None, id_col_name=None, query=None, index=None, nprobe=None, model=None, topk=20):
@@ -23,13 +23,11 @@ def search_FAISSIndex(data=None, id_col_name=None, query=None, index=None, nprob
     ids = [i for i in I][0]
 
     print(ids)
-    L2_score = [d for d in D][0]
-    inner_product = [calculate_inner_product(l2) for l2 in L2_score]
+    inner_product = [d for d in D][0]
 
     search_result = pd.DataFrame()
     search_result["id"] = ids
     search_result['cosine_sim'] = inner_product
-    search_result['L2_score'] = L2_score
 
     if data is not None:
         dat = data[data[id_col_name].isin(ids)]
@@ -43,16 +41,7 @@ def search_FAISSIndex(data=None, id_col_name=None, query=None, index=None, nprob
 
 def query_swe_bert(query):
     print("Bert query:\n" + query)
-    index = faiss.read_index("./wine/wine_data_collect/movies_desc.index")
-    # from wine_data_collect import preparedata
-    # vinbeskrivningar = preparedata.collect_all_sentences()
-    model_name = 'KBLab/sentence-bert-swedish-cased'
-    model = SentenceTransformer(model_name)
-    # query = "drycken heter crémant d'alsace chardonnay. denna dryck smakar fruktig, mycket frisk, nyanserad smak med inslag av päron, gula äpplen, rostat bröd, honungsmelon och grapefrukt..den säljs buteljerad på flaska. drycken har en organisk klassificering 1.drycken har en alkoholprocent på 12.5%. drycken har en volym på 375 ml. dryckens pris är 99.0. drycken kommer från landet frankrike. drycken är av kategorin mousserande vin och underkategorin torrt vitt. drycken rekommenderas serveras serveras vid 8-10°c som aperitif, eller till rätter av fisk eller skaldjur, eller till sallader..den passar som eller till aperitif, fisk, skaldjur, grönsaker. drycken har förslutits genom naturkork.druvorna är gjort på druvornachardonnay..dryckens färgton är ljusgul färg.. per 100 milliliter är sockermängden 0.9"
-    # print(query)
     search_result = search_FAISSIndex( id_col_name="id", query=query, index=index, nprobe=10, model=model, topk=20)
-    # search_result = search_result[['id', vinbeskrivning', 'cosine_sim', 'L2_score']]
-    # print(search_result)
     combined_list = []
     id_list = [id + 1 for id in search_result['id']]
     for distance, _ids in zip(search_result['cosine_sim'], id_list):
